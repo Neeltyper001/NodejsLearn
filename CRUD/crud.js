521d379637071bc9fbb008f70b665f1a70a7fcd8
Neelesh Joshi <neeleshjoshi001@gmail.com>
2023-01-20 09:49:10 +0530

    updated the ingredients of crud.js

--- CRUD/crud.js
+++ CRUD/crud.js
@@ -5,3 +5,11 @@ const pth = require('path');
 
 const targetpth = pth.join(__dirname, '/sampledir');
 console.log(targetpth);
+
+// createFile
+try{
+    fs.writeFileSync(`${targetpth}`+`/creatfile.txt`,'This is the created file');    
+}
+catch (iferr){
+    console.log('Oops thats an error name: '+iferr);
+}
